9d63eda38e556f31ca255c8d5f0c9b7176acc6a3
alextheoctopus <sashabeznosova2002@mail.ru>
2022-09-10 17:55:50 +0400

    р

--- client/src/components/UI/MyButton.js
+++
@@ -1,8 +0,0 @@
-/* import React from "react";
-import '../header/Header.css';
-const MyButton=(props)=>{
-    return(
-        <button >{props.children}</button>
-    )
-}
-export default MyButton; */
--- client/src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authStore from "./features/authStore/authStore";
-export default configureStore(
-    {
-        reducer: {
-            auth:authStore,
-        }
-    }
-);
